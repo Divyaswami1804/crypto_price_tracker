aace7b1c1f368a06ecb3ba1db2ce4103f0300c07
Divya Swami <divyaswami1804@gmail.com>
2025-07-16 21:27:21 +0530

    added html form for crypto input and display result

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=['Get', 'POST'])
 def home():
-    return "Hello crypto world !"
+    coin_name = None
+    if request.method == 'POST':
+        coin_name = request.form['coin']
+    return render_template('index.html',coin_name = coin_name )
+
 if __name__ == '__main__':
     app.run(debug=True)
